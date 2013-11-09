48407429a86ac1900cb5aad3a42e4e75d28c4136
Matt Nathan <matt.nathan+github@gmail.com>
2013-11-09 09:48:06 +0000

    Remove unused method (and reformat code)

--- chapi-map/chapi-map-core/src/main/java/org/chapi/map/Main.java
+++ chapi-map/chapi-map-core/src/main/java/org/chapi/map/Main.java
@@ -19,27 +19,27 @@ public class Main {
     protected void configure() {
       // map the target path /data to the result of the endpoint given by the url
       map("/data")
-              .from(TestEndpoints.class).urlShortener();
+          .from(TestEndpoints.class).urlShortener();
 
       // map specific fields from a named endpoint
       map("/user/")
-              .toFields("name,title,phone,address")
-              .from(TestEndpoints.class).urlShortener();
+          .toFields("name,title,phone,address")
+          .from(TestEndpoints.class).urlShortener();
 
       map("/user/avatar")
-              .toProjection(new Projection<BackendData, ApiData>() {
-                @Override
-                public ApiData project(BackendData from) {
-                  ApiData apiData = new ApiData();
-                  apiData.uri = from.href;
-                  return apiData;
-                }
-              })
-              .from(TestEndpoints.class).urlShortener();
+          .toProjection(new Projection<BackendData, ApiData>() {
+            @Override
+            public ApiData project(BackendData from) {
+              ApiData apiData = new ApiData();
+              apiData.uri = from.href;
+              return apiData;
+            }
+          })
+          .from(TestEndpoints.class).urlShortener();
 
       // map to a constant
       map("/kind")
-              .toConstant("example#kind");
+          .toConstant("example#kind");
     }
   }
 
--- chapi-map/chapi-map-core/src/main/java/org/chapi/map/mapper/DataSourceMappingBuilder.java
+++ chapi-map/chapi-map-core/src/main/java/org/chapi/map/mapper/DataSourceMappingBuilder.java
@@ -4,8 +4,6 @@ package org.chapi.map.mapper;
  * Created by Matt on 07/11/13.
  */
 public interface DataSourceMappingBuilder {
-  DataExtractMappingBuilder toEndpoint(String endpoint);
-
   <T> T from(Class<T> endpointDescription);
 
   void toConstant(String constant);
