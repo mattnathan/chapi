406159398a4947cc187e9be2780741b6c7c6a24c
Matt Nathan <matt.nathan+github@gmail.com>
2013-11-10 14:04:08 +0000

    Refactor the constant value tests to make them easier to add more tests to it

--- chapi-map/chapi-map-core/src/test/java/org/chapi/map/MapiTest.java
+++ chapi-map/chapi-map-core/src/test/java/org/chapi/map/MapiTest.java
@@ -1,20 +1,33 @@
 package org.chapi.map;
 
 import com.google.api.client.util.Lists;
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.chapi.map.spi.ConstantValueMapping;
 import org.chapi.map.spi.DefaultMappingSourceVisitor;
 import org.chapi.map.spi.Mapping;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
+import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Maps.uniqueIndex;
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests for different types of graph mappings.
  */
 public class MapiTest {
+
+  private static final Function<Mapping, String> SOURCE_PATH_FUNCTION = new Function<Mapping, String>() {
+    @Override
+    public String apply(Mapping input) {
+      return input.getSourcePath();
+    }
+  };
+
   @Test
   public void testNoGraphsGivesNoMappings() throws Exception {
     Projector projector = Mapi.createProjector();
@@ -37,22 +50,27 @@ public class MapiTest {
     ConstantMappingGraph graph = new ConstantMappingGraph();
     Projector projector = Mapi.createProjector(graph);
     graph.verifyExpectedMappings(projector);
-
   }
 
   private static class ConstantMappingGraph extends AbstractGraph {
+    private final Map<String, Expected> expectedMappings = newHashMap();
 
     public void verifyExpectedMappings(Projector projector) {
       List<ConstantValueMapping> constantMappings = collectConstantMappings(projector);
-      assertEquals(1, constantMappings.size());
-      ConstantValueMapping mapping = constantMappings.get(0);
-      assertEquals("/constants/string", mapping.getSourcePath());
-      assertEquals("CONSTANT_STRING", mapping.getValue());
+      ImmutableMap<String, ConstantValueMapping> map = uniqueIndex(constantMappings, SOURCE_PATH_FUNCTION);
+
+      assertEquals(expectedMappings.keySet(), map.keySet());
+      for (Map.Entry<String, ConstantValueMapping> entry : map.entrySet()) {
+        expectedMappings.get(entry.getKey()).validate(entry.getValue());
+      }
     }
 
     @Override
     protected void configure() {
       map("/constants/string").toConstant("CONSTANT_STRING");
+      expectedMappings.put("/constants/string", new Expected("CONSTANT_STRING"));
+      map("/constants/string2").toConstant("CONSTANT_STRING2");
+      expectedMappings.put("/constants/string2", new Expected("CONSTANT_STRING2"));
     }
 
     private List<ConstantValueMapping> collectConstantMappings(Projector projector) {
@@ -65,6 +83,18 @@ public class MapiTest {
         }
       }
       return constantMappings;
+    }
+
+    private static class Expected {
+      private final String value;
+
+      private Expected(String value) {
+        this.value = value;
+      }
+
+      public void validate(ConstantValueMapping mapping) {
+        assertEquals(value, mapping.getValue());
+      }
     }
 
     private static class ConstantMappingPredicate extends DefaultMappingSourceVisitor<ConstantValueMapping> {
